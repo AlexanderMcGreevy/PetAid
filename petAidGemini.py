4798a363499a6f4f46dc48822b9d90eb4cce935c
EMSaltz <ethansaltz4359@gmail.com>
2025-03-29 20:24:15 -0400

    Update petAidGemini.py

--- petAidGemini.py
+++ petAidGemini.py
@@ -1,12 +1,21 @@
 #Values in variables are placeholders
-
 from google import genai
 from dotenv import load_dotenv
+from flask import Flask, jsonify
 import os
 
-load_dotenv(".env")
-print(os.getenv("GEMINI_API_KEY"))
-client = genai.Client(api_key=f"{os.getenv("GEMINI_API_KEY")}")
+
+app = Flask(__name__)
+@app.route('/')
+def diagnose():
+    return jsonify(response.text)
+
+
+load_dotenv('env')
+y=os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=f"{y}")
+
 
 
 
@@ -29,7 +38,7 @@ pet_information={
 if species=='dog':
     pet_information['breed']=breed
 
-pet_health_question = input('Please put your question: ')
+pet_health_question = 'example'
 
 pet_health_question = 'pet health question: ' + pet_health_question
 
@@ -44,4 +53,7 @@ response_info = "Phrase it in this template, don't say anything along the lines 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents=[pet_information_list,response_info,pet_health_question, 'You are an a digital assistant for vetarinary affairs. Given the information and question, make a suggestion on what the issue is. Make a percent likelihood and severity. If likelyhood is below 15 percent, do not add to final report. Do not add anything about treatment, only if you should see a vet.', 'Phrase in neutral tone, though not too clinical.']
 )
-print(response.text)
+
+
+if __name__ == '__main__':
+    app.run()
